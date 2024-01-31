516843f279cbfda92862eb63d48d48a26bbdd0f5
peet <112712989+PETROUNKNOWN@users.noreply.github.com>
2024-02-01 01:30:55 +0300

    V1.0
    First script version.

---
+++ caps.py
@@ -0,0 +1,7 @@
+def convert_to_uppercase(text):
+    return text.upper()
+
+if __name__ == "__main__":
+    input_text = "text here"
+    result = convert_to_uppercase(input_text)
+    print("Converted Text:", result)
